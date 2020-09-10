17f42d38060dd5e6d2f20db90ed256edfe867f6b
krela2010 <diao.ke@163.com>
2020-09-10 20:09:13 +0800

    initilize

---
+++ DYLiveStream/DYLiveStream/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DYLiveStream
+//
+//  Created by 老刁 on 2020/9/10.
+//  Copyright © 2020 老刁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
